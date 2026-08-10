8fd832efd8cafbca26bde4f0d3d49662da02b994
Harrison M. Ndugba BCS <ndu.harry02@gmail.com>
2026-08-10 08:12:37 +0100

    main: chore: add quotes

---
+++ frontend/src/types/quote.ts
@@ -0,0 +1,11 @@
+export type Quote = {
+  id: number;
+  vehicleId: number;
+  driverAge: number;
+  yearsLicensed: number;
+  penaltyPoints: number;
+  durationMinutes: number;
+  pricePence: number;
+  expiresAt: string;
+  createdAt: string;
+};
