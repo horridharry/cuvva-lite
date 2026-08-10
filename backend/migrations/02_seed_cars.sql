e82938a1716e0322975df4b046a0488780862d17
Harrison M. Ndugba BCS <ndu.harry02@gmail.com>
2026-08-10 01:22:27 +0100

    main: backend: migration + vehicle seed data

---
+++ backend/migrations/02_seed_cars.sql
@@ -0,0 +1,24 @@
+INSERT INTO vehicles (
+    reg,
+    make,
+    model,
+    year,
+    fuel_type,
+    engine_size_cc
+)
+VALUES
+    ('FH18 UKU', 'Toyota', 'AURIS', 2018, 'Hybrid', 1798),
+    ('GK21 LNP', 'Volkswagen', 'GOLF', 2021, 'Petrol', 1498),
+    ('LM19 XRD', 'BMW', '3 SERIES', 2019, 'Diesel', 1995),
+    ('YT17 KCV', 'Ford', 'FOCUS', 2017, 'Petrol', 999),
+    ('RX22 JHG', 'Mercedes-Benz', 'A CLASS', 2022, 'Petrol', 1332),
+    ('KP20 WTZ', 'Audi', 'A3', 2020, 'Diesel', 1968),
+    ('DN16 PLO', 'Honda', 'CIVIC', 2016, 'Petrol', 1798),
+    ('WV23 MCK', 'Nissan', 'QASHQAI', 2023, 'Hybrid', 1332),
+    ('BG15 HXR', 'Vauxhall', 'CORSA', 2015, 'Petrol', 1398),
+    ('SJ19 FVA', 'Hyundai', 'TUCSON', 2019, 'Diesel', 1598),
+    ('NA22 RYE', 'Kia', 'SPORTAGE', 2022, 'Hybrid', 1598),
+    ('CE14 UBM', 'Mazda', 'MAZDA3', 2014, 'Petrol', 1998),
+    ('PX20 GJD', 'Volvo', 'XC40', 2020, 'Petrol', 1969),
+    ('HG18 ZKL', 'Renault', 'CLIO', 2018, 'Diesel', 1461),
+    ('AV23 TNS', 'Tesla', 'MODEL 3', 2023, 'Electric', 0);
