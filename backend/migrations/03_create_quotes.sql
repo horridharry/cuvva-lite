b4231aa3d0f8fc49c36077098cc94ce337a250a4
Harrison M. Ndugba BCS <ndu.harry02@gmail.com>
2026-08-10 02:26:25 +0100

    main: db: add quotes table

---
+++ backend/migrations/03_create_quotes.sql
@@ -0,0 +1,12 @@
+CREATE TABLE quotes (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    vehicle_id BIGINT NOT NULL
+        REFERENCES vehicles(id),
+    driver_age SMALLINT NOT NULL,
+    years_licensed SMALLINT NOT NULL,
+    penalty_points SMALLINT NOT NULL,
+    duration INTEGER NOT NULL,
+    price_pence INTEGER NOT NULL,
+    expires_at TIMESTAMPTZ NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
